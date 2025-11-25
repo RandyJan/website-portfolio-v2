3a9cfba2d15407b22954c7b0a9118e55e5ebf502
Randy Jan Rongcales <randyjan250@gmail.com>
2025-11-25 16:25:21 +0800

    updated

--- app/segment/Hero.tsx
+++ app/segment/Hero.tsx
@@ -10,9 +10,9 @@ const Hero = () => {
   const {is_dark} = GlobalStore();
   return (
     <div className="relative container flex flex-col sm:pl-28 pt-10 sm:pt-40 h-full max-w-full overflow-hidden" id="home">
+      <div className="absolute -top-20 -left-20 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob"></div>
       {/* Floating Background Shapes */}
       <BgParticles key={is_dark}  />
-      <div className="absolute -top-20 -left-20 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob"></div>
       <div className="absolute -bottom-20 -right-10 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-2000"></div>
 
       <div className="flex flex-col-reverse sm:flex-row items-center justify-around w-full overflow-x-hidden">
